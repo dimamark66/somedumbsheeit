0a3a7fb3047320672a9fd5ada709f42f938b5878
Sareff <ilbezr@gmail.com>
2020-03-24 20:41:24 +0300

    First steps

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let text = document.getElementById("decryptedText");
+let encText = document.getElementById("encryptedText");
+let encrypt = document.getElementById("encrypt");
+let decrypt = document.getElementById("decrypt");
+
+encrypt.onclick = function() {
+  encText.value = "fuck you";
+}
+
